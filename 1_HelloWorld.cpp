49269b5727cdda03651fa37a6f189f4fd3238c90
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2020-07-10 13:18:04 +0530

    Hello World Program

---
+++ 1_HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello World";
+	return 0;
+}
+
+//C++ Introduction 
+//
+//	1. Low level programming
+//	2. Speed of execution
+//	3. Derived from C
+//	4. Richer library than C
+//	5. Object Oriented Programming
